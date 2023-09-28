39ee92a6eceab796639ea6a89757ddcdb0bd8ec3
David Campbell <tryggus20@gmail.com>
2023-09-28 15:29:12 -0500

    pushing

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,21 @@
 import PizzaMenu from "../PizzaMenu/PizzaMenu";
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { useSelector, } from "react-redux";
+
+
 export default function Home(displayPizza) {
   const pList = useSelector((store) => store.pizzaReducer);
   console.log(pList);
 
+  useEffect(() => {
+      displayPizza();
+}, []);
+
   return (
     <div>
       {pList.map((name, id) => {
-        <PizzaMenu key={id} />;
+        <PizzaMenu key={id} name={name} />;
       })}
     </div>
   );
