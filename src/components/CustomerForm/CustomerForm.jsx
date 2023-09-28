0dc127069203e789f5891b7536e6b95472a9176c
Abdi <abdullahia0215@gmail.com>
2023-09-28 15:21:51 -0500

    layout finished, minor tweaking needed

--- src/components/CustomerForm/CustomerForm.jsx
+++ src/components/CustomerForm/CustomerForm.jsx
@@ -1,5 +1,99 @@
-export default function CustomerForm(){
-    return(
-        <p>hello</p>
-    )
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+export default function CustomerForm() {
+    const dispatch = useDispatch();
+    const [nameToAdd, setNameToAdd] = useState('');
+    const [addressToAdd, setAddressToAdd] = useState('');
+    const [cityToAdd, setCityToAdd] = useState('');
+    const [zipToAdd, setZipToAdd] = useState('');
+    const [receptionMethod, setReceptionMethod] = useState('');
+
+    const handleNameChange = (event) => {
+        setNameToAdd(event.target.value);
+    };
+
+    const handleAddressChange = (event) => {
+        setAddressToAdd(event.target.value);
+    };
+
+    const handleCityChange = (event) => {
+        setCityToAdd(event.target.value);
+    };
+
+    const handleZipChange = (event) => {
+        setZipToAdd(event.target.value);
+    };
+
+    const handleReceptionMethodChange = (event) => {
+        setReceptionMethod(event.target.value);
+    };
+
+    const addCustomerInfo = (event) => {
+        event.preventDefault();
+        const infoToAdd = {
+            name: nameToAdd,
+            address: addressToAdd,
+            city: cityToAdd,
+            zip: zipToAdd,
+            receptionMethod: receptionMethod
+        };
+        dispatch({ type: 'ADD_NEW_CUSTOMER_INFO', payload: infoToAdd });
+
+        addNewInfo(infoToAdd);
+    };
+
+    const addNewInfo = (info) => {
+        console.log('Adding customer info:', info);
+    };
+
+    return (
+        <form onSubmit={addCustomerInfo}>
+            <input
+                onChange={handleNameChange}
+                type="text"
+                placeholder="Name"
+                required
+            />
+
+            <input
+                onChange={handleAddressChange}
+                type="text"
+                placeholder="Address"
+                required
+            />
+
+            <input
+                onChange={handleCityChange}
+                type="text"
+                placeholder="City"
+                required
+            />
+
+            <input
+                onChange={handleZipChange}
+                type="text"
+                placeholder="Zip Code"
+                required
+            />
+
+            <input
+                type="radio"
+                name="receptionMethod"
+                value="delivery"
+                onChange={handleReceptionMethodChange}
+                required
+            /><label>Delivery</label>
+
+            <input
+                type="radio"
+                name="receptionMethod"
+                value="pickup"
+                onChange={handleReceptionMethodChange}
+                required
+            /><label>Pickup</label>
+
+            <button type="submit">Next</button>
+        </form>
+    );
 }
