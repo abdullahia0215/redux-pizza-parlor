b03da2025abc4abc8b0e2671384cd34c2d0b04a4
Abdi <abdullahia0215@gmail.com>
2023-09-28 14:02:11 -0500

    added needed componens and redux

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,83 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Switch, NavLink } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import Home from '../Home/Home';
+import CustomerForm from '../CustomerForm/CustomerForm';
+import Checkout from '../Checkout/Checkout';
+import Admin from './Admin/Admin';
 
 function App() {
+  const dispatch = useDispatch();
+const displayPizza = () => {
+  axios.get("/api/pizza/")
+  .then((response) => {
+    console.log(response.data);
+    dispatch({ type: "SET_PIZZA", payload: response.data });
+  })
+  .catch((error) => {
+    console.log("error on GET to display pizza reducer", error);
+  });
+}
 
+const displayOrder = () => {
+  axios.get("/api/order/")
+  .then((response) => {
+    console.log(response.data);
+    dispatch({ type: "SET_ORDER", payload: response.data });
+  })
+  .catch((error) => {
+    console.log("error on GET to display order reducer", error);
+  });
+}
+
+const postOrder = () => {
+  axios.post("/api/order/")
+  .then((response) => {
+    console.log(response.data);
+    dispatch({ type: "ADD_ORDER", payload: response.data });
+  })
+  .catch((error) => {
+    console.log("error on POST to add order in order reducer", error);
+  });
+}
+
+
+
+
+
+  
   return (
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Prime Pizza</h1>
       </header>
-  
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
-  
+      <Router>
+        <ul>
+<li><NavLink exact to="/">Home</NavLink></li>
+<li><NavLink exact to="/customer">Customer Information</NavLink></li>
+<li><NavLink exact to="/checkout">Checkout</NavLink></li>
+        </ul>
+        <Switch>  
+        <Route path="/">
+          {/* <Home /> */}
+          <p>Home Component is here</p>
+        </Route>
+        <Route path="/customer">
+          {/* <CustomerForm /> */}
+          <p>Customer Form goes here</p>
+        </Route>
+        <Route path="/checkout">
+          {/* <Checkout/> */}
+          <p>Checkout Component goes here</p>
+        </Route>
+        <Route path="/admin">
+          {/* <ADMIN /> */}
+          <p>Hidden Admin Page Goes here. No peeking!</p>
+        </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const pizzaReducer = (state = [], action) => {
+    switch (action.type) {
+      case "SET_PIZZA":
+        return action.payload;
+      default:
+        return state;
+    }
+  };
+
+  const orderReducer = (state = [], action) => {
+    switch (action.type) {
+      case "SET_ORDER":
+        return action.payload;
+        case "ADD_ORDER":
+            return [...state, action.payload];
+      default:
+        return state;
+    }
+  };
+
+
+const store = createStore(
+  combineReducers({
+    orderReducer,
+    pizzaReducer
+  }),
+  applyMiddleware(logger)
+);
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
+  document.getElementById("root")
+);
+
